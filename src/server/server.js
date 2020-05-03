e641672e1e99508c9b3b83bcd23b55d9b815ee99
Marek Sagan <marek.sagan@protonmail.com>
2020-05-03 19:07:10 +0200

    Added Node.js server script

---
+++ src/server/server.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var createError = require('http-errors');
+
+var contactsRouter = require('./routes/contacts');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/contacts', contactsRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+res.json({
+  message: err.message,
+  error: err
+});
+});
+
+app.listen(3000, () => {
+  console.log('Express.js is listening to port 3000');
+});
+
+module.exports = app;
